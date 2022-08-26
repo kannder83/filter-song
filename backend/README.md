afeb070ab2214d05fa9f32b3c691334f67a0549c
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-08-25 22:06:03 -0500

    configuration basis for the project

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+# Backend
+
+This backend uses fastapi and postgres.
+
+## Create file .env:
+
+```
+DATABASE_HOSTNAME=postgres
+DATABASE_PORT=5432
+DATABASE_PASSWORD=iwillbebetter123*
+DATABASE_NAME=songs_db
+DATABASE_USERNAME=postgres
+POSTGRES_PASSWORD=iwillbebetter123*
+POSTGRES_DB=songs_db
+DEBUG=True
+ALLOWED_HOSTS=localhost:8000
+```
---
+++ backend/config/app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Database
+# from app.users.models import Base
+# from config.database import engine
+
+# Routes
+from app.routes import router as router_songs
+
+
+def get_application():
+
+    # create tables in database
+    # Base.metadata.create_all(bind=engine)
+
+    app = FastAPI(
+        title="filter-song",
+        description="Search the best songs",
+        docs_url="/",
+        version="0.1.0"
+    )
+
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router_songs)
+
+    return app
+
+
+application = get_application()
