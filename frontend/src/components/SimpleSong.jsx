c1d7fa8a9dd1f6f746cf9d2f96a9cacf4a80bffc
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-08-27 15:18:35 -0500

    fix key id

--- frontend/src/components/SimpleSong.jsx
+++ frontend/src/components/SimpleSong.jsx
@@ -8,11 +8,8 @@ const SimpleSong = ({ songs }) => {
       <ul className="w-full">
         {songs.map((song) => {
           return (
-            <Link to={`/library/${song.id}`}>
-              <li
-                className="w-full p-1 my-1 rounded-md flex justify-center bg-gray-100 hover:bg-gray-100/80 cursor-pointer"
-                key={song.id}
-              >
+            <Link key={song.id} to={`/library/${song.id}`}>
+              <li className="w-full p-1 my-1 rounded-md flex justify-center bg-gray-100 hover:bg-gray-100/80 cursor-pointer">
                 {song.song} - {song.artist}
               </li>
             </Link>
