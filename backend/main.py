5347e68fa8abb57e5aad33e0a7d707fa20da37d3
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-09-18 20:01:33 -0500

    fix prod file

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
 import uvicorn
+from subprocess import run
+from config.conf import settings
 
 
 def main():
     """
-    Main for runs a fastapi server
+    Main: It runs a fastapi server
     """
-    uvicorn.run(
-        "config.app:application",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-    )
+    if settings.debug == True:
+        uvicorn.run(
+            "config.app:application",
+            host="0.0.0.0",
+            port=8000,
+            reload=True,
+        )
+    else:
+        run("gunicorn config.app:application -w 3 -b 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker".split(' '))
 
 
 if __name__ == "__main__":
